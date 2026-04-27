880f3d035d493e13fbb591316ffebeecd1e29ba0
Vitor Brosque <vitorbrosque@gmail.com>
2026-04-27 12:35:04 -0300

    Add vehicle model

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Vehicle
 
-# Register your models here.
+
+@admin.register(Vehicle)
+class VehicleAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "brand",
+        "model",
+        "version",
+        "price",
+        "category",
+        "enrichment_status",
+        "created_at",
+    )
+    search_fields = ("raw_input", "brand", "model", "version")
+    list_filter = ("category", "enrichment_status", "created_at")
