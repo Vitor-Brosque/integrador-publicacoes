f19439150e2d8a71a7ca8b7224f1d01c4554d9e1
Vitor Brosque <vitorbrosque@gmail.com>
2026-04-28 11:09:23 -0300

    Add admin action to generate organic posts

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,4 +1,6 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+
+from posts.services.post_pipeline import run_post_pipeline
 from .models import Vehicle
 
 
@@ -16,3 +18,18 @@ class VehicleAdmin(admin.ModelAdmin):
     )
     search_fields = ("raw_input", "brand", "model", "version")
     list_filter = ("category", "enrichment_status", "created_at")
+    actions = ("generate_organic_post",)
+
+    @admin.action(description="Gerar post orgânico")
+    def generate_organic_post(self, request, queryset):
+        created_count = 0
+
+        for vehicle in queryset:
+            run_post_pipeline(vehicle)
+            created_count += 1
+
+        self.message_user(
+            request,
+            f"{created_count} post(s) orgânico(s) gerado(s) com sucesso.",
+            level=messages.SUCCESS,
+        )
