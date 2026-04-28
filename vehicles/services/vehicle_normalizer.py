b5af2b4f18c838ce49601a52a614285e6d7d148e
Vitor Brosque <vitorbrosque@gmail.com>
2026-04-28 12:12:17 -0300

    Refactor vehicle normalizer to use AI parser layer

--- vehicles/services/vehicle_normalizer.py
+++ vehicles/services/vehicle_normalizer.py
@@ -1,23 +1,26 @@
+from ai.vehicle_parser import parse_vehicle
 from vehicles.models import Vehicle
 
 
 def normalize_vehicle(vehicle: Vehicle):
-    raw = vehicle.raw_input.lower()
+    parsed_data = parse_vehicle(vehicle.raw_input)
 
-    if "toyota" in raw:
-        vehicle.brand = "Toyota"
+    vehicle.brand = parsed_data.brand
+    vehicle.model = parsed_data.model
+    vehicle.version = parsed_data.version
+    vehicle.manufacture_year = parsed_data.manufacture_year
+    vehicle.model_year = parsed_data.model_year
+    vehicle.mileage = parsed_data.mileage
+    vehicle.color = parsed_data.color
+    vehicle.doors = parsed_data.doors
+    vehicle.transmission = parsed_data.transmission
+    vehicle.fuel = parsed_data.fuel
+    vehicle.category = parsed_data.category
 
-    if "etios" in raw:
-        vehicle.model = "Etios"
-
-    if "hatch" in raw:
-        vehicle.category = "hatch"
-
-    if "manual" in raw:
-        vehicle.transmission = "Manual"
-
-    if "flex" in raw:
-        vehicle.fuel = "Flex"
+    vehicle.usage_profile = parsed_data.usage_profile
+    vehicle.target_audience = parsed_data.target_audience
+    vehicle.commercial_positioning = parsed_data.commercial_positioning
+    vehicle.enrichment_source = parsed_data.enrichment_source
 
     vehicle.normalized_by_ai = True
     vehicle.enrichment_status = "basic"
