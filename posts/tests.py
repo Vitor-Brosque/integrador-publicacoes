1804bc37e6e8ee47a9499f50062b53f34081f8aa
Vitor Brosque <vitorbrosque@gmail.com>
2026-04-29 15:18:28 -0300

    Add duplicate prevention pipeline test

--- posts/tests.py
+++ posts/tests.py
@@ -21,3 +21,20 @@ Hatch"""
         self.assertEqual(post.vehicle, vehicle)
         self.assertEqual(post.platform_posts.count(), 5)
         self.assertEqual(post.review.status, "pending")
+
+    def test_run_post_pipeline_does_not_duplicate_pending_post(self):
+        vehicle = Vehicle.objects.create(
+            raw_input="""Volkswagen Gol
+    1.0 FLEX MANUAL
+    R$ 39.900
+    2018/2019
+    Branco
+    4 portas
+    Hatch"""
+        )
+
+        first_post = run_post_pipeline(vehicle)
+        second_post = run_post_pipeline(vehicle)
+
+        self.assertEqual(first_post.id, second_post.id)
+        self.assertEqual(vehicle.social_posts.count(), 1)
