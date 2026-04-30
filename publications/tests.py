0422411599d53f2db3a3e22f62e5bdc02c85b0cd
Vitor Brosque <vitorbrosque@gmail.com>
2026-04-30 14:26:04 -0300

    Add fake publication publisher test

--- publications/tests.py
+++ publications/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
-# Create your tests here.
+from posts.services.post_pipeline import run_post_pipeline
+from publications.services.publication_creator import create_publication_targets
+from publications.services.publisher import publish_to_platform
+from vehicles.models import Vehicle
+
+
+class PublicationPublisherTest(TestCase):
+    def test_publish_to_platform_marks_publication_as_published(self):
+        vehicle = Vehicle.objects.create(
+            raw_input="""Volkswagen Gol
+1.0 FLEX MANUAL
+R$ 39.900
+2018/2019
+Branco
+4 portas
+Hatch"""
+        )
+
+        post = run_post_pipeline(vehicle)
+
+        post.review.status = "approved"
+        post.review.save()
+
+        publication_targets = create_publication_targets(post)
+        publication = publication_targets[0]
+
+        publish_to_platform(publication)
+
+        publication.refresh_from_db()
+
+        self.assertEqual(publication.status, "published")
+        self.assertNotEqual(publication.external_post_id, "")
+        self.assertNotEqual(publication.external_url, "")
+        self.assertIsNotNone(publication.published_at)
