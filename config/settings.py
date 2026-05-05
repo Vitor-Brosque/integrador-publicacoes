e56aa14bf30efbb5718c29828f5e4e46d8f88793
Vitor Brosque <vitorbrosque@gmail.com>
2026-05-05 15:46:11 -0300

    Prepare manual public URL media flow

--- config/settings.py
+++ config/settings.py
@@ -31,14 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
     "vehicles",
-    "media_library",
+    "media_library.apps.MediaLibraryConfig",
     "posts",
     "social_accounts",
     "publications",
--- media_library/admin.py
+++ media_library/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from media_library.models import MediaAsset
+
+
+@admin.register(MediaAsset)
+class MediaAssetAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "vehicle",
+        "media_type",
+        "file",
+        "public_url",
+        "created_at",
+    )
+    search_fields = (
+        "vehicle__brand",
+        "vehicle__model",
+        "file",
+        "public_url",
+    )
+    list_filter = (
+        "media_type",
+        "created_at",
+    )
